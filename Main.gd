255cf75296011c8a62b5fa8f3fc7a8f5e7cbd4cc
Gerald Landl <glandl@duality.at>
2022-04-26 17:05:40 +0200

    HUD added

--- Main.gd
+++ Main.gd
@@ -12,12 +12,15 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_MobTimer_timeout():
 	var mob = mob_scene.instance()
@@ -39,6 +42,7 @@ func _on_MobTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score()
 
 
 func _on_StartTimer_timeout():
